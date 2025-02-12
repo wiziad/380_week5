52b648dc5b462a29588a4df29a66f02e3ebad0d7
Gauthier Appaix <98077233+Gauthier7k@users.noreply.github.com>
2025-02-12 12:03:49 -0700

    car

--- Lab-Week5/edu/ucalgary/oop/Driver.java
+++ Lab-Week5/edu/ucalgary/oop/Driver.java
@@ -1,6 +1,21 @@
 package edu.ucalgary.oop;
 
+class Driver {
+    private String name;
 
-public class Driver {
-    
+    public Driver(String name) {  //Driver("Alice")
+        this.name = name;
+    }
+
+    public Driver(Driver other) {  //deep copy, for new driver test code 
+        this.name = other.name;
+    }
+
+    public String getName() { //for assert equals
+        return name;
+    }
+
+    public void setName(String name) { //update name, for assert false
+        this.name = name;
+    }
 }
