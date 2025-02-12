4df655cf5c2e0fd9988321ef7c061ab31d24d1ea
Gauthier Appaix <98077233+Gauthier7k@users.noreply.github.com>
2025-02-12 12:07:56 -0700

    all tests pass yayayayay

--- Lab-Week5/edu/ucalgary/oop/Car.java
+++ Lab-Week5/edu/ucalgary/oop/Car.java
@@ -1,51 +1,45 @@
 package edu.ucalgary.oop;
 
-public class Car {
-	
-	private String model;
-	private Driver driver;
-	private Engine engine;
-	
-	public Car (String model, Driver driver, Engine engine) {
-	    this.model = model;
-	    this.driver = driver;
-	    this.engine = engine;
-	  }
-	
-	public Car (Car other, Boolean deepCopy) {
-		this.model = other.model; 
+class Car {
+    private String model;
+    private Driver driver;
+    private Engine engine;
 
-	    if (deepCopy) {
-	        this.driver = new Driver(other.driver);  
-	        this.engine = new Engine(other.engine);  
-	    } else {
-	        this.driver = other.driver; 
-	        this.engine = other.engine;
-	    }
-	}
-	
-	public String getModel() {
-		return model;
-	}
-	
-	public void setModel(String model) {
-		this.model = model;
-	}
-	
-	public Driver getDriver() {
-		return driver;
-	}
-	
-	public void setDriver(Driver driver) {
-		this.driver = driver;
-	}
-	
-	public Engine getEngine() {
-		return engine;
-	}
-	
-	public void setEngine(Engine engine) {
-		this.engine = engine;
-	}
-	
+    public Car(String model, Driver driver, Engine engine) {
+        this.model = model;
+        this.driver = driver;
+        this.engine = engine;
+    }
+
+    public Car copy(boolean deepCopy) {
+        if (deepCopy) {
+            return new Car(this.model, new Driver(this.driver), new Engine(this.engine));
+        } else {
+            return new Car(this.model, this.driver, this.engine);
+        }
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
 }
