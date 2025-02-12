f0586b35a55c30501e370431b5e8b430dfc9ea90
Gauthier Appaix <98077233+Gauthier7k@users.noreply.github.com>
2025-02-12 11:44:36 -0700

    engine class done

--- Lab-Week5/edu/ucalgary/oop/Engine.java
+++ Lab-Week5/edu/ucalgary/oop/Engine.java
@@ -1,6 +1,33 @@
 package edu.ucalgary.oop;
 
-
 public class Engine {
-    
+    private String engineType;
+
+    // Constructor
+    public Engine(String engineType) {
+        this.engineType = engineType;
+    }
+
+    // Copy constructor for deep copy
+    public Engine(Engine other) {
+        this.engineType = other.engineType;
+    }
+
+    // Getter for engineType
+    public String getEngine() {
+        return engineType;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    // Setter for engineType
+    public void setEngine(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
 }
